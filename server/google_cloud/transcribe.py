0c915728351be8f14d91b02a7d3c4ff92ee12f3c
Grace Liu <graceliu.gll@gmail.com>
2024-01-27 02:47:36 -0500

    jan 27 commit

---
+++ server/google_cloud/transcribe.py
@@ -0,0 +1,38 @@
+
+def transcribe_gcs(gcs_uri: str) -> str:
+    """Asynchronously transcribes the audio file specified by the gcs_uri.
+
+    Args:
+        gcs_uri: The Google Cloud Storage path to an audio file.
+
+    Returns:
+        The generated transcript from the audio file provided.
+    """
+    from google.cloud import speech
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code="en-CAN",
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    transcript_builder = []
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        transcript_builder.append(f"\nTranscript: {result.alternatives[0].transcript}")
+        transcript_builder.append(f"\nConfidence: {result.alternatives[0].confidence}")
+
+    transcript = "".join(transcript_builder)
+    print(transcript)
+
+    return transcript
