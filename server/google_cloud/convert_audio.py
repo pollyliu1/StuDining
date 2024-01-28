b5a0cfcc1fb52d043a434b6f0d50fd849777f002
Grace Liu <graceliu.gll@gmail.com>
2024-01-27 22:17:04 -0500

    change convert_audio.py so that the output wav file has sample_width=2

--- server/google_cloud/convert_audio.py
+++ server/google_cloud/convert_audio.py
@@ -1,9 +1,13 @@
-# import moviepy.editor as moviepy
 from pydub import AudioSegment
+import audioop
 
 def convert_webm_to_wav():
     audio = AudioSegment.from_file("../audio.webm", format="webm")
-    # audio = AudioSegment.from_file("/Users/graceliu/Downloads/big-buck-bunny_trailer.webm", format="webm")
+    frame_rate = audio.frame_rate
+    channels = audio.channels
+
+    audio = audioop.lin2lin(audio.raw_data, audio.sample_width, 2)
+    audio = AudioSegment(audio, frame_rate=frame_rate, channels=channels, sample_width=2)
     audio.export("/Users/graceliu/UofTHacksXI/server/test_out.wav", format="wav")
 
 if __name__ == "__main__":
